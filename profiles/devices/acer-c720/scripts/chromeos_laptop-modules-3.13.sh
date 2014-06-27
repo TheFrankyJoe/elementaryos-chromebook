17b55fb6c9775dbe3cac87667674283d779c284c
Jonathan Frank <chronomania40@gmail.com>
2014-06-27 07:21:23 -0400

    Added pre-compiled ChromeOS laptop modules for Kernel 3.13

---
+++ profiles/devices/acer-c720/scripts/chromeos_laptop-modules-3.13.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+modules_url="http://sourceforge.net/projects/chromeeos/files/profiles/devices/chromeos_laptop/chromeos_laptop-modules-3.13.tar.gz/download"
+modules_archives="chromeos_laptop-modules.tar.gz"
+
+echo "Downloading ChromeOS laptop modules for kernel 3.13..."
+curl -o "$modules_archives" -L -O "$modules_url"
+
+echo "Extracting ChromeOS laptop modules..."
+tar -xvf $modules_archives
+
+echo "Installing ChromeOS laptop modules..."
+cd chromeos_laptop/
+cp -f chromeos_laptop.ko /lib/modules/3.13.0-30-generic/kernel/drivers/platform/chrome/
+cp -f i2c-designware*.ko /lib/modules/3.13.0-30-generic/kernel/drivers/i2c/
+
+echo "ChromeOS laptop modules installation finished..."
+exit 0
